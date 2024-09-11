c138c5f6ed67c059834463dc4aff91f39af313fc
Razan <rznjamal@gmail.com>
2024-09-11 16:39:07 +0300

    Smartzone Commit Razan 11-09-2024 04:39 PM

--- database/migrations/2024_08_21_081354_add_image_to_insurances_table.php
+++ database/migrations/2024_08_21_081354_add_image_to_insurances_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('insurances', function (Blueprint $table) {
-            $table->dropColumn('image');
+          
         });
     }
 };
